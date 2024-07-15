9247d390e6911476a8573750e8653b9f6813ffbb
Samuel <samuelint@gmail.com>
2024-07-15 13:34:36 -0400

    refact: cleanup

--- tests/test_classic_rag.py
+++ tests/test_classic_rag.py
@@ -2,20 +2,14 @@ import pytest
 from rag_compare.classic_rag import ClassicRag
 from langchain_community.retrievers import WikipediaRetriever
 from tests.llm import all_llms
-import time
 
 retriever = WikipediaRetriever(top_k_results=6, doc_content_chars_max=2000)
 
 
 @pytest.mark.parametrize("llm", all_llms, ids=[llm._llm_type for llm in all_llms])
 def test_classic_rag(llm):
-
-    start = time.time()
     chain = ClassicRag(llm=llm, retriever=retriever).build()
     result = chain.invoke({"input": "How fast are cheetahs?"})
-    end = time.time()
-    exec_time = f"Execution time: {end - start:.2f} seconds"
-    print(exec_time)
 
     assert result["input"] == "How fast are cheetahs?"
     assert len(result["context"]) > 0
